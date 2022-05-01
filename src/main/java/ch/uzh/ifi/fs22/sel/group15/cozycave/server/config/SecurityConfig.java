be680371c782c1ff35ae469cbf8d32674e627524
Lukas Herz <mail@lukasherz.de>
2022-05-01 17:59:52 +0200

    disable security

--- src/main/java/ch/uzh/ifi/fs22/sel/group15/cozycave/server/config/SecurityConfig.java
+++ src/main/java/ch/uzh/ifi/fs22/sel/group15/cozycave/server/config/SecurityConfig.java
@@ -4,18 +4,16 @@ import ch.uzh.ifi.fs22.sel.group15.cozycave.server.security.JwtAuthenticationEnt
 import ch.uzh.ifi.fs22.sel.group15.cozycave.server.security.JwtAuthenticationFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.BeanIds;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-@Configuration
-@EnableWebSecurity
+//@Configuration
+//@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
